96bdba35832358a85d71ea107466c0294c9e7ffb
sekharlee <51740389+sekharlee@users.noreply.github.com>
2022-12-06 16:30:12 +0530

    Update autosubenum.sh

--- autosubenum.sh
+++ autosubenum.sh
@@ -8,11 +8,11 @@ echo -e "\n$bar\n\tSubdomains Enumerator\n$bar\n"
 read -p "Please enter target (i.e. site.com): " domain
 
 HOST=$domain
-subfinderPATH=/root/results/$HOST-subfinder.txt
+
 
 function automateSubfinder() {
     echo -e "\n$bar\n\tRunning Subfinder\n$bar\n"
-    subfinder -o $subfinderPATH -t 100 -d $HOST 
+    subfinder -o domains.txt -t 100 -d $HOST 
 }
 
 automateSubfinder $HOST
